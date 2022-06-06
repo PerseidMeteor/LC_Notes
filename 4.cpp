821b086a2428e456215596aaecbb44601dc05f6b
lanlanlanlan <y_q_email@163.com>
2022-06-06 14:43:30 +0800

    asd

--- 4.cpp
+++ 4.cpp
@@ -8,6 +8,7 @@ public:
         int m = nums1.size();
         int n = nums2.size();
         
+        //asdasdj
         if(m == 0)
             return n % 2 == 1 ? nums2[n/2]:(double)(nums2[n/2] + nums2[(n-1)/2])/2;
         
